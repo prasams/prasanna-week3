8d6197cb613bc3d5c38f3403ae3d5d8d8e2bb503
prasams <prasanna.v@gmail.com>
2024-04-22 06:09:38 +0000

    Initial commit

--- watcher.sh
+++ watcher.sh
@@ -1,1 +1,10 @@
-#TODO
+#bash script to monitor the kubernetes deployment for the swype app.
+
+#variable declarations
+NameSpace="sre"
+DeploymentName ="swype-app"
+MaxRestarts = 3
+
+#starting the loop and run it indefinetly 
+
+while true; do
